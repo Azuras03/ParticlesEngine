24b49c5f1b4c0d376107c533617787b4eaa0b469
Azuras03 <nr03russo@gmail.com>
2023-01-22 16:32:50 +0100

    Ajout de la classe main

--- src/main/java/nr03/particlesengine/ParticleEngine.java
+++ src/main/java/nr03/particlesengine/ParticleEngine.java
@@ -1,21 +1,24 @@
 package nr03.particlesengine;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import nr03.particlesengine.Vue.VueScreen;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ParticleEngine extends Application {
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
     @Override
     public void start(Stage stage) throws IOException {
-        Pane root = new Pane();
-        Scene scene = new Scene(root, 320, 240);
+        VueScreen particleScreen = new VueScreen();
+        Scene scene = new Scene(particleScreen, WIDTH, HEIGHT);;
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
