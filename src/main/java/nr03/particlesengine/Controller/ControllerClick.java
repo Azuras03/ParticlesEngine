908ddc40a56dd420207157b8d300fc283f6b38b6
Azuras03 <nr03russo@gmail.com>
2023-01-22 18:09:14 +0100

    Mise à jour des controllers en fonction des changements dans les vues

--- src/main/java/nr03/particlesengine/Controller/ControllerClick.java
+++ src/main/java/nr03/particlesengine/Controller/ControllerClick.java
@@ -3,6 +3,7 @@ package nr03.particlesengine.Controller;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import nr03.particlesengine.ParticleEngine;
 import nr03.particlesengine.Vue.VueClick;
 import nr03.particlesengine.Vue.VueParticle;
 import nr03.particlesengine.Vue.VueScreen;
@@ -20,7 +21,7 @@ public class ControllerClick implements EventHandler<MouseEvent> {
         vS.getChildren().add(new VueClick(event.getX(), event.getY(), color));
         color = color.brighter();
         for (int i = 0; i < 50; i++) {
-            VueParticle vueParticle = new VueParticle(event.getX(), event.getY(), 5, Math.random() * 2 * Math.PI, Math.random() * 10, color);
+            VueParticle vueParticle = new VueParticle(event.getX(), event.getY(), ParticleEngine.radiusBalls, Math.random() * 2 * Math.PI, Math.random() * 10, color);
             vS.getChildren().add(vueParticle);
             vS.getParticles().add(vueParticle);
         }
--- src/main/java/nr03/particlesengine/Controller/ControllerDrag.java
+++ src/main/java/nr03/particlesengine/Controller/ControllerDrag.java
@@ -1,8 +1,10 @@
 package nr03.particlesengine.Controller;
 
 import javafx.event.EventHandler;
+import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import nr03.particlesengine.ParticleEngine;
 import nr03.particlesengine.Vue.VueClick;
 import nr03.particlesengine.Vue.VueParticle;
 import nr03.particlesengine.Vue.VueScreen;
@@ -20,7 +22,7 @@ public class ControllerDrag implements EventHandler<MouseEvent> {
         vS.getChildren().add(new VueClick(event.getX(), event.getY(), color));
         color = color.brighter();
         for (int i = 0; i < 5; i++) {
-            VueParticle vueParticle = new VueParticle(event.getX(), event.getY(), 5, Math.random() * 2 * Math.PI, Math.random() * 10, color);
+            VueParticle vueParticle = new VueParticle(event.getX(), event.getY(), ParticleEngine.radiusBalls, Math.random() * 2 * Math.PI, Math.random() * 10, color);
             vS.getChildren().add(vueParticle);
             vS.getParticles().add(vueParticle);
         }
